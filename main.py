758ac8dbc6a5b17e6e6896956bc86b7bf2942607
Plotnikov VD <pltnikovqwerty@gmail.com>
2024-06-23 16:35:12 +0300

    Fetching as discord.ext.commands.command

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import discord
 import pandas as pd
+from discord.ext import commands
 from dotenv import dotenv_values
 from re import fullmatch
 
 
-class MyClient(discord.Client):
+class MyClient(commands.Bot):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
-        # await self.change_presence(status=discord.Status.invisible)
-    
-    async def on_message(self, message: discord.Message):
-        if (mtch := fullmatch(r'!fetch\s?([\d]*)', message.content)):
+        await self.change_presence(status=discord.Status.invisible)
+
+    def add_commands(self):
+        @self.command(name='fetch')
+        async def fetch(ctx, limit=10):
+            message = ctx.message
             await message.delete()
-            limit = int(mtch.groups()[0]) if mtch.groups()[0] else 10
             print(f'Last {limit} messages in {message.channel.guild}/{message.channel}')
             async for msg in client.get_partial_messageable(message.channel.id).history(limit=limit):
                 print(msg.content)
-            
 
 
 if __name__ == "__main__":
@@ -29,5 +30,6 @@ if __name__ == "__main__":
     intents.guild_messages = True
     intents.guilds = True
 
-    client = MyClient(intents=intents)
+    client = MyClient(command_prefix='!', intents=intents)
+    client.add_commands()
     client.run(config['API_TOKEN'])
