9c45ace36955826d01a9b926aab773ecfba652db
Plotnikov VD <pltnikovqwerty@gmail.com>
2024-06-23 16:13:02 +0300

    Bot command to fetch messages

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import pandas as pd
 from dotenv import dotenv_values
+from re import fullmatch
 
 
 class MyClient(discord.Client):
@@ -9,9 +10,13 @@ class MyClient(discord.Client):
         # await self.change_presence(status=discord.Status.invisible)
     
     async def on_message(self, message: discord.Message):
-
-        async for msg in client.get_partial_messageable(message.channel.id).history(limit=10):
-            print(msg.content)
+        if (mtch := fullmatch(r'!fetch\s?([\d]*)', message.content)):
+            await message.delete()
+            limit = int(mtch.groups()[0]) if mtch.groups()[0] else 10
+            print(f'Last {limit} messages in {message.channel.guild}/{message.channel}')
+            async for msg in client.get_partial_messageable(message.channel.id).history(limit=limit):
+                print(msg.content)
+            
 
 
 if __name__ == "__main__":
@@ -19,6 +24,8 @@ if __name__ == "__main__":
     intents = discord.Intents.default()
 
     intents.message_content = True
+    intents.messages = True
+
     intents.guild_messages = True
     intents.guilds = True
 
