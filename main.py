b3390fa2fdcefc6702369690c4ca6573ca654daa
Plotnikov VD <pltnikovqwerty@gmail.com>
2024-06-26 12:59:26 +0300

    Logging usage

--- main.py
+++ main.py
@@ -1,25 +1,49 @@
 from typing import Any
 import discord
 from dotenv import dotenv_values
+import logging
+
+formatter = logging.Formatter(r'%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+
+ds_handler = logging.FileHandler('discord.log', 'w')
+ds_handler.setFormatter(formatter)
+ds_handler.setLevel(logging.INFO)
+ds_logger = logging.getLogger('discord')
+ds_logger.addHandler(ds_handler)
+ds_logger.setLevel(logging.DEBUG)
+
+logger = logging.getLogger(__name__)
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+stream_handler.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler('main.log', 'w')
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 class MyClient(discord.Client):
     def __init__(self, **options: Any) -> None:
+        logger.debug('Bot created')
         intents = discord.Intents.default()
         intents.message_content = True
 
         super().__init__(intents=intents, **options)
 
     async def on_ready(self):
-        print(f'Logged on as {self.user}!')
+        logger.info(f'Logged on as %s!', self.user)
         await self.change_presence(status=discord.Status.invisible)
     
     async def on_message(self, message: discord.Message):
-        print(message.content)
+        logger.info('Message handled: %s', message.content)
 
 
 if __name__ == "__main__":
     config = dotenv_values('.env')
-    
+
     client = MyClient()
-    client.run(config['API_TOKEN'])
+    client.run(config['API_TOKEN'], log_handler=None)
